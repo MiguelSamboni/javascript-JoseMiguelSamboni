70d1e2265db035b48389179941312be0e55efdea
Miguel Samboni <josemiguelsamboni9@gmail.com>
2025-02-18 11:17:51 -0500

    se agregó javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+//ejercicio 1 
+let nombre = "Miguel Samboni";
+let edad = "19";
+let ciudad = "Gigante Huila";
+console.log ("Mi nombre es" + nombre + ",tengoo" + edad + "años y vivo en")
+
+/* crea una variable llamada precio y asignale un valor decimal. Luego crea una variable llamada cantidad y asignaleun valor entero.
+Muestra el tipo de dato de cada variable en la consola.*/
+
+let precio = 3.14;
+let cantidad = 5;
+console.log (typeof precio); // imprime en la consola
+console. write (typeof cantidad); //imprime en el navegador 
+alert(typeof cantidad); // imprime en una ventana emergente
